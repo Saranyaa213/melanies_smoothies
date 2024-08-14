4ffee3b3a7454ae0abe8d1ebec1a2b963024c9f3
Saranyaa213 <162974218+Saranyaa213@users.noreply.github.com>
2024-08-14 17:21:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,12 @@ st.write(
 )
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("Name on your Smoothie will be:", name_on_order)
-search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
-#pd_df=my_dataframe.to_pandas()
+pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
