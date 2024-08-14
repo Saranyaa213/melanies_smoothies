ad609286e5a31bfb797ba960e65e6d80431e3402
Saranyaa213 <162974218+Saranyaa213@users.noreply.github.com>
2024-08-14 17:19:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-pd_df=my_dataframe.to_pandas()
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+#pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
